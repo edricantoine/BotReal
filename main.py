4dcd79a6743305e2ad173998212e77044cb3ff83
Edric Antoine <edric.antoine@gmail.com>
2022-01-03 13:50:40 -0800

    First commit

---
+++ main.py
@@ -0,0 +1,92 @@
+# bot.py
+import os
+import discord
+import requests
+import json
+import random
+import twentyOne
+
+client = discord.Client()
+twentyOne = twentyOne.twentyOne(client)
+happyWords = ["angry", "mad", "trash", "bad", "loser", "garbage", "pissed", "frustrated"]
+gameBeingPlayed = False
+sadStatements = [
+    "Consider suicide",
+    "Nobody cares that you feel that way",
+    "Cope, seethe, mald",
+    "Doin ur mom",
+    "Cry more"
+]
+GUILD = os.getenv('Bot test')
+
+
+@client.event
+async def on_ready():
+    print('Logged in as {0.user}'.format(client))
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            break
+
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})'
+    )
+
+    members = '\n - '.join([member.name for member in guild.members])
+    print(f'Guild Members:\n - {members}')
+
+
+@client.event
+async def on_message(message):
+    global gameBeingPlayed
+    if message.author == client.user:
+        return
+
+    msg = message.content
+
+    if msg.startswith('!hello'):
+        await message.channel.send('Howdy')
+
+    if msg.startswith('!exit'):
+        await message.channel.send('Aight imma head out')
+        await client.close()
+        exit()
+
+    if msg.startswith('!21') and not gameBeingPlayed:
+        await twentyOne.start(message.channel)
+        gameBeingPlayed = True
+
+    if msg.startswith('!stop21'):
+        await twentyOne.setChannel(message.channel)
+        await twentyOne.stop()
+        gameBeingPlayed = False
+
+    if msg.startswith('!score21'):
+        await twentyOne.setChannel(message.channel)
+        await twentyOne.print()
+
+    if msg.startswith('!hit') and twentyOne.started():
+        if twentyOne.isp1turn():
+            await twentyOne.onehit()
+            await twentyOne.changeTurntoP2()
+            await twentyOne.turn2()
+        else:
+            await twentyOne.twohit()
+            await twentyOne.changeTurntoP1()
+            await twentyOne.addToScores()
+
+    if msg.startswith('!stay') and twentyOne.started():
+        if twentyOne.isp1turn():
+            await twentyOne.onestay()
+            await twentyOne.changeTurntoP2()
+            await twentyOne.turn2()
+        else:
+            await twentyOne.twostay()
+            await twentyOne.changeTurntoP1()
+            await twentyOne.addToScores()
+
+    if any(word in msg for word in happyWords):
+        await message.channel.send(random.choice(sadStatements))
+
+
+client.run('OTI3MzUxNTI2NjM1MjI1MDk4.YdI9aQ.B8pkHyI6Wn7YLdkGq575zQlIhAE')
---
+++ twentyOne.py
@@ -0,0 +1,114 @@
+import random
+import os
+import asyncio
+
+
+class twentyOne:
+
+    def __init__(self, client):
+        self.__running = False
+        self._client = client
+        self.__channel = None
+        self.__p1turn = True
+        self.__1hors = None
+        self.__2hors = None
+        self.__s1 = 0
+        self.__s2 = 0
+
+    async def setChannel(self, channel):
+        self.__channel = channel
+
+    def started(self):
+        return self.__running
+
+    def isp1turn(self):
+        return self.__p1turn
+
+    async def onehit(self):
+        self.__1hors = True
+
+    async def twohit(self):
+        self.__2hors = True
+
+    async def onestay(self):
+        self.__1hors = False
+
+    async def twostay(self):
+        self.__2hors = False
+
+    async def changeTurntoP2(self):
+        self.__p1turn = False
+
+    async def changeTurntoP1(self):
+        self.__p1turn = True
+
+    async def start(self, channel):
+        if self.__running:
+            await self.__channel.send('A game of 21 is in progress already, in channel {}. You can '
+                                      'stop '
+                                      'it with !stop.')
+        else:
+            await self.reset()
+            self.__channel = channel
+            await self.__channel.send('@here Starting game of 21!')
+            self.__running = True
+            await self.turn1()
+
+    async def turn1(self):
+
+        await self.print()
+        await self.__channel.send('Player 1. Will you hit or stay?')
+
+    async def turn2(self):
+        await self.__channel.send('Player 2. Will you hit or stay?')
+
+    async def addToScores(self):
+        rand1 = random.randrange(1, 10)
+        rand2 = random.randrange(1, 10)
+
+        if self.__1hors:
+            self.__s1 += rand1
+        if self.__2hors:
+            self.__s2 += rand2
+
+        await self.checkForWinner()
+
+    async def checkForWinner(self):
+        if self.__s1 > 21 and self.__s2 <= 21:
+            await self.print()
+            await self.__channel.send('Player 1 went over 21! Player 2 wins.')
+            await self.stop()
+
+        elif self.__s2 > 21 and self.__s1 <= 21:
+            await self.print()
+            await self.__channel.send('Player 2 went over 21! Player 1 wins.')
+            await self.stop()
+        elif self.__s2 > 21 and self.__s1 > 21:
+            await self.print()
+            await self.__channel.send('Both players went over 21! Too bad! Nobody wins.')
+            await self.stop()
+        else:
+            await self.turn1()
+
+    async def reset(self):
+        if self.__running:
+            await self.stop()
+        self.__s1 = 0
+        self.__s2 = 0
+        self.__p1turn = True
+
+    async def stop(self):
+        if self.__running:
+            await self.__channel.send('Stopping game. Final scores are...')
+            await self.print()
+            self.__running = False
+        else:
+            await self.__channel.send('No game has been started yet! Start one with "!21".')
+
+    async def print(self):
+        if self.__running:
+            await self.__channel.send('Game scores:')
+            await self.__channel.send('Player 1: {}'.format(self.__s1))
+            await self.__channel.send('Player 2: {}'.format(self.__s2))
+        else:
+            await self.__channel.send('No game is currently active.')
